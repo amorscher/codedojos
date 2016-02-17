f40f5887a63005e5b0543154ef6454c5fcc6d623
A. Morscher <Am.morscher@gmail.com>
2016-02-17 02:24:50 +0100

    added overall test suite

---
+++ at.mcorp.quicksort/test/at/mcorp/quicksort/AllQuickSortTests.java
@@ -0,0 +1,12 @@
+package at.mcorp.quicksort;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({QuickSortTest
+		.class, QuickSortTestWithTheories.class})
+public class AllQuickSortTests {
+//overall test suite
+}
