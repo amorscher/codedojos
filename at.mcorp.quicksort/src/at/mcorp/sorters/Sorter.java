2b9330c42d3473c95523e97c55284dd43ecb6ad8
ambrosmorscher <Am.Morscher@gmail.com>
2016-06-13 20:22:45 +0200

    reverted the test commit

--- at.mcorp.quicksort/src/at/mcorp/sorters/Sorter.java
+++ at.mcorp.quicksort/src/at/mcorp/sorters/Sorter.java
@@ -2,7 +2,7 @@ package at.mcorp.sorters;
 
 /**
  * Common interface for all sorters providing a sort method taking an order
- * attribute. All sorters have to full-fill this contract. Some more comment.
+ * attribute. All sorters have to full-fill this contract.
  * 
  * @author ambros
  *
